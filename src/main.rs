f0ccb347440dc16aa67c31d3c7fa32b74a5afcf7
dutch <gregory_matous@yahoo.com>
2023-10-11 23:48:05 -0500

    Enums

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
+use server::Server;
+use http::Request;
+
+mod server;
+mod http;
+
 fn main() {
-    let addr = String::from("192.168.0.0:8080");
+    let addr = String::from("127.0.0.1:8080");
     let port = &addr[10..];
-    let server = Server::new("addr".to_string());
+    let server = Server::new(addr.to_string());
     server.run();
 
-    let x = dbg!(port);
-}
-
-struct Server {
-    address: String,
-}
-
-impl Server {
-    fn new(address: String) -> Server {
-        Self { address }
-    }
-
-    fn run(self) {
-        println!("Server is listening..")
-    }
-}
- struct Request {
-     path: String,
-     query_string: Option<String>,
-     method: Method,
- }
-
-enum Method {
-    GET(String),
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
+    //let x = dbg!(port);
 }
